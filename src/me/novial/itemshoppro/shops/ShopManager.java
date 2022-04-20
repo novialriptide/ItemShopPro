3d76cfa0422e396edb80e48ca01f68127a11e235
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2022-04-20 01:01:55 -0400

    Create ShopManager.java

---
+++ src/me/novial/itemshoppro/shops/ShopManager.java
@@ -0,0 +1,15 @@
+package me.novial.itemshoppro.shops;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+
+public class ShopManager {
+    public Shop[] shops;
+
+    public ShopManager() {
+
+    }
+
+    public void loadShopsFromYml(YamlConfiguration ymlFile) {
+
+    }
+}
