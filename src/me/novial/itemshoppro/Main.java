dea787cedff14c361b0e66c5fbdbfd2bac848759
Andrew Hong <35881688+NovialRiptide@users.noreply.github.com>
2022-04-27 12:09:32 -0400

    add createFiles()

--- src/me/novial/itemshoppro/Main.java
+++ src/me/novial/itemshoppro/Main.java
@@ -5,19 +5,51 @@ import me.novial.itemshoppro.listeners.CreateShopSign;
 import me.novial.itemshoppro.listeners.LockChest;
 import me.novial.itemshoppro.listeners.PlayerInteractShopSign;
 import me.novial.itemshoppro.objects.ShopManager;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Main extends JavaPlugin {
     public static ShopManager shopManager = new ShopManager();
 
+    private File shopsFile;
+    private FileConfiguration shopsConfig;
+
     @Override
     public void onEnable() {
-        this.getServer().getConsoleSender().sendMessage("Loading commands...");
-        this.getCommand("itemshoppro").setExecutor((new CommandItemShopPro()));
+        this.getServer().getConsoleSender().sendMessage("Creating/loading files...");
+        createFiles();
 
-        this.getServer().getConsoleSender().sendMessage("Loading event listeners...");
-        this.getServer().getPluginManager().registerEvents(new CreateShopSign(), this);
-        this.getServer().getPluginManager().registerEvents(new PlayerInteractShopSign(), this);
-        this.getServer().getPluginManager().registerEvents(new LockChest(), this);
+        ConsoleCommandSender commandSender = getServer().getConsoleSender();
+        PluginManager pluginManager = getServer().getPluginManager();
+
+        commandSender.sendMessage("Loading commands...");
+        getCommand("itemshoppro").setExecutor((new CommandItemShopPro()));
+
+        commandSender.sendMessage("Loading event listeners...");
+        pluginManager.registerEvents(new CreateShopSign(), this);
+        pluginManager.registerEvents(new PlayerInteractShopSign(), this);
+        pluginManager.registerEvents(new LockChest(), this);
+    }
+
+    private void createFiles() {
+        shopsFile = new File(getDataFolder(), "shops.yml");
+        if (!shopsFile.exists()) {
+            shopsFile.getParentFile().mkdirs();
+            saveResource("shops.yml", false);
+        }
+
+        shopsConfig = new YamlConfiguration();
+        try {
+            shopsConfig.load(shopsFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
     }
 }
