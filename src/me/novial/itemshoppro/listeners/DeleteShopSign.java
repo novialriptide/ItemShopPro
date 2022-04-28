3c6d08677d02a6405721079d1dae1c656352958d
Andrew Hong <35881688+NovialRiptide@users.noreply.github.com>
2022-04-28 09:03:26 -0400

    implement shop deletion

--- src/me/novial/itemshoppro/listeners/DeleteShopSign.java
+++ src/me/novial/itemshoppro/listeners/DeleteShopSign.java
@@ -1,4 +1,40 @@
 package me.novial.itemshoppro.listeners;
 
-public class DeleteShopSign {
+import me.novial.itemshoppro.Main;
+import me.novial.itemshoppro.objects.Shop;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Chest;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+import java.io.IOException;
+
+public class DeleteShopSign implements Listener {
+    @EventHandler
+    public void onBlockDestroy(BlockBreakEvent event) throws IOException {
+        Player player = event.getPlayer();
+        Block block = event.getBlock();
+        BlockState blockState = block.getState();
+        Shop shop;
+
+        if (blockState instanceof Chest) {
+            shop = Main.shopManager.findShopFromChest((Chest) blockState);
+        }
+        else if (blockState instanceof Sign) {
+            shop = Main.shopManager.findShopFromSign((Sign) blockState);
+        }
+        else {
+            return;
+        }
+
+        Main.shopManager.shops.remove(shop);
+        Main.shopsConfig.set("{0}".format(player.getUniqueId().toString()), null);
+        Main.shopsConfig.save(Main.shopsFile);
+
+        player.sendMessage("Deleted shop.");
+    }
 }
